d1c11bccd1dd9a64bf5bdd0dc32f0d6c3d53388c
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-09 04:52:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   attr_accessor()
-  attr_reader()
+  attr_reader(:name, :artist, :genre)
 
-  def initialize(song)
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
   end
+
 end
